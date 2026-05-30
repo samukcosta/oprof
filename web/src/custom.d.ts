05ecef75e154602f5c6d4bc20c649316f38c4b4c
Samuel Costa <samu.ks@outlook.com>
2026-05-30 03:25:33 -0300

    Necessário para reconhecimento de alguns arquivos no projeto.

---
+++ web/src/custom.d.ts
@@ -0,0 +1,10 @@
+declare module "*.css"
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
